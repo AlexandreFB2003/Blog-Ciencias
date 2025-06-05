9c6ffc12f535432dc259cb9d33af9e2eedcaad97
AlexandreFB2003 <afborges2003@gmail.com>
2025-06-05 17:38:44 -0300

    Página em construção

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Se encontra em estado de desenvolvimento um blog que ensina sobre ciências de uma forma simples</h1>
+    </div>
+  );
+}
